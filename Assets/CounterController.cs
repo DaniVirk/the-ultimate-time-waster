79467cf8b48cd3a32261c0eb7fe26c657cff3a3e
Danial <dani2630dk@gmail.com>
2025-05-09 23:45:17 +0200

    Added back to menu

--- Assets/CounterController.cs
+++ Assets/CounterController.cs
@@ -2,28 +2,29 @@ using UnityEngine;
 using System.Timers;
 using TMPro;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 using Object = System.Object;
 
 public class TimerToText : MonoBehaviour
 {
-    private Timer _timer;
-    private static long _counter;
     public TextMeshProUGUI timerText;
-    private int _days;
-    private int _hours;
-    private int _minutes;
-    private int _seconds;
+    public Button mainButton;
+    
+    private Timer _timer;
+    
+    private static long _counter;
     private Vector2 _mousePos;
+    private bool _gameOver;
 
     private void Start()
     {
+        _counter = 0;
+        mainButton.gameObject.SetActive(false);
         _mousePos = Mouse.current.position.ReadValue();
-        
-        Debug.Log(_mousePos);
         _timer = new Timer(1000);
-        _timer.Elapsed += OnTimedEvent; // Hook up the event
-        _timer.AutoReset = true; // Restart timer after each event
-        _timer.Enabled = true; // Start the timer
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;
+        _timer.Enabled = true;
     }
 
     private void OnDestroy()
@@ -33,51 +34,51 @@ public class TimerToText : MonoBehaviour
 
     private void Update()
     {
-        if (AnyKeyPressed())
+        if (_gameOver) return;
+
+        if (AnyKeyPressed() && _gameOver == false)
         {
-            Debug.Log("You moved!");
             _timer.Enabled = false;
+            mainButton.gameObject.SetActive(true);
+            _gameOver = true;
         }
         PrintTimerToText();
     }
 
     private void PrintTimerToText()
     {
-        _days = (int)_counter / (24 * 3600);
-        _hours = (int)(_counter % (24 * 3600)) / 3600;
-        _minutes = (int)(_counter % 3600) / 60;
-        _seconds = (int)_counter % 60;
+        var days = (int)_counter / (24 * 3600);
+        var hours = (int)(_counter % (24 * 3600)) / 3600;
+        var minutes = (int)(_counter % 3600) / 60;
+        var seconds = (int)_counter % 60;
         
         var timer = "";
 
-        if (_days != 0)
+        if (days != 0)
         {
-            timer += $"{_days}:";
+            timer += $"{days}:";
         }
 
-        if (_days != 0 || _hours != 0)
+        if (days != 0 || hours != 0)
         {
-            timer += $"{_hours:D2}:";
+            timer += $"{hours:D2}:";
         }
-        else if (_minutes != 0)
+        else if (minutes != 0)
         {
-            // Skip hours but still want mm:ss
-            timer += $"{_minutes:D2}:";
-        }
-
-        if (_days != 0 || _hours != 0)
-        {
-            timer += $"{_minutes:D2}:";
+            timer += $"{minutes:D2}:";
         }
 
-        timer += $"{_seconds:D2}";
-
-        timerText.text = timer;
+        if (days != 0 || hours != 0)
+        {
+            timer += $"{minutes:D2}:";
+        }
+        
+        timerText.text = timer + $"{seconds:D2}";
     }
 
     private bool AnyKeyPressed()
     {
-        if (Keyboard.current.anyKey.isPressed) return true;
+        if (Keyboard.current.anyKey.isPressed){ return true;}
         if (Mouse.current.leftButton.isPressed) return true;
         if (Mouse.current.rightButton.isPressed) return true;
         if (Mouse.current.position.ReadValue() != _mousePos) return true;
